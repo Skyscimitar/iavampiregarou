5bda48ceae08895257c841a5f5483940b44a67a6
LNBaud <44223448+LNBaud@users.noreply.github.com>
2019-02-18 18:37:17 +0100

    Test +modif de la connexion avec le serveur

--- src/connectionServer.py
+++ src/connectionServer.py
@@ -10,73 +10,81 @@ def getcommand(sock):
     return commande.decode()
 
 
-def getresult(sock):
+def getint(sock):
     data = bytes()
-    while len(data) != 8:
-        data += sock.recv(8 - len(data))
-    return struct.unpack("d", data)[0]
+    data += sock.recv(1)
+    #print(data)
+    return int.from_bytes(data, byteorder='big')
 
 
 def understand_set_command(sock):
-    n = getresult(sock)
-    m = getresult(sock)
-
+    n = getint(sock)
+    m = getint(sock)
     return n, m
 
 
 def understand_hum_command(sock):
-    nb_homes = getresult(sock)
+    nb_homes = getint(sock)
 
     coordinates = []
     for i in range(nb_homes):
-        x = getresult(sock)
-        y = getresult(sock)
+        x = getint(sock)
+        y = getint(sock)
         coordinates += [(x,y)]
 
     return coordinates
 
 
 def understand_hme_command(sock):
-    x = getresult(sock)
-    y = getresult(sock)
+    x = getint(sock)
+    y = getint(sock)
 
     return x,y
 
 
 def understand_upd_command(sock):
-    nb_changes = getresult(sock)
+    nb_changes = getint(sock)
 
     coordinates = []
     for i in range(nb_changes):
-        x = getresult(sock)
-        y = getresult(sock)
-        humans = getresult(sock)
-        vampires = getresult(sock)
-        werewolves = getresult(sock)
-        coordinates += [(x,y)]
+        x = getint(sock)
+        y = getint(sock)
+        humans = getint(sock)
+        vampires = getint(sock)
+        werewolves = getint(sock)
+        coordinates += [(x,y,humans,vampires,werewolves)]
 
     return coordinates
 
 
-def send_nme_command(sock, command, name):
+def send_nme_command(sock, name):
     paquet = bytes()
-    paquet += command.encode()
-    paquet += struct.pack("d", name.length)
+    paquet += 'NME'.encode()
+    paquet += struct.pack("d", len(name))
     paquet += name.encode()
     sock.send(paquet)
 
 
-def send_mov_command(sock, command, movements):
+def send_mov_command(sock, movements):
     paquet = bytes()
-    paquet += command.encode()
-    paquet += struct.pack("d", movements.length)
+    paquet += 'MOV'.encode()
+    paquet += bytes([len(movements)])
+    print(movements)
 
     for movement in movements:
-        paquet += struct.pack("d", movement[0])
-        paquet += struct.pack("d", movement[1])
-        paquet += struct.pack("d", movement[2])
-        paquet += struct.pack("d", movement[3])
-        paquet += struct.pack("d", movement[4])
+        print(movement)
+        print(movement[0])
+        paquet += bytes([movement[0]])
+        paquet += bytes([movement[1]])
+        paquet += bytes([movement[2]])
+        paquet += bytes([movement[3]])
+        paquet += bytes([movement[4]])
+        print(paquet)
+        #paquet += struct.pack("d", movement[0])
+        #paquet += struct.pack("d", movement[1])
+        #paquet += struct.pack("d", movement[2])
+        #paquet += struct.pack("d", movement[3])
+        #paquet += struct.pack("d", movement[4])
 
     sock.send(paquet)
 
@@ -84,27 +92,34 @@ def send_mov_command(sock, command, movements):
 if __name__ == '__main__':
     # connexion au server
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("localhost", 555))
+    sock.connect(("localhost", 5555))
+    print('Connecté')
+    send_nme_command(sock, 'helene')
 
     while True:
-        cmd = getcommand(sock)
 
-        if cmd == u"SET":
-            print ("SET command:", understand_set_command(sock))
-        elif cmd == u"HUM":
-            print("HUM command:", understand_hum_command(sock))
-        elif cmd == u"HME":
-            print("HME command:", understand_hme_command(sock))
-        elif cmd == u"MAP":
-            print("MAP command:", understand_upd_command(sock))
-        elif cmd == u"UPD":
-            print("UPD command:", understand_upd_command(sock))
-        elif cmd == u"END":
-            break
-        elif cmd == u"BYE":
-            sleep(1)
-            continue
-        else:
-            raise ValueError("Erreur protocole")
+        while True :
+            cmd = getcommand(sock)
+            #print('commande reçue :' + cmd )
+            if cmd == u"SET":
+                print ("SET command:", understand_set_command(sock))
+            elif cmd == u"HUM":
+                print("HUM command:", understand_hum_command(sock))
+            elif cmd == u"HME":
+                print("HME command:", understand_hme_command(sock))
+            elif cmd == u"MAP":
+                print("MAP command:", understand_upd_command(sock))
+            elif cmd == u"UPD":
+                print("UPD command:", understand_upd_command(sock))
+                break
+            elif cmd == u"END":
+                break
+            elif cmd == u"BYE":
+                sleep(1)
+                continue
+            else:
+                raise ValueError("Erreur protocole")
+
+        send_mov_command(sock,[(4,3,4,4,4)])
 
         sleep(1)

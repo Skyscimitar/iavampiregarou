b5dcea167c804a4b483f987b98d641dcc0727a5e
sebastien-lb <sebast.lubineau@laposte.net>
2019-03-04 13:00:32 +0100

    add map

--- src/decider.py
+++ src/decider.py
@@ -11,8 +11,11 @@ def next_moves_decider(game_state):
         #return scoring_function(state, game_state.team_specie, 20, -3, 1)
         return scoring_function_2(state, game_state.team_specie, 20, -10)
     print("Deciding move for specie " + str(game_state.team_specie))
-    #moves, bestScore = alphabeta(game_state, 5, scoring, get_next_states)
-    moves, bestScore = alphabeta(game_state, 4, scoring, stupidNext)
+    if len(game_state.humans) <= 2:
+        moves, bestScore = alphabeta(game_state, 2, scoring, stupidNext)
+    else:
+        #moves, bestScore = alphabeta(game_state, 4, scoring, stupidNext)
+        moves, bestScore = alphabeta(game_state, 4, scoring, get_next_states)
     print("next move decided with alpha beta score:" + str(bestScore) + "  move:")
     for m in moves:
         print(m)
